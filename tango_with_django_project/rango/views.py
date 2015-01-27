4ff30c147b0ae9d3f44a9b988c28e4e8b9393a9b
seanmac10 <sean.mclaughlin427@gmail.com>
2015-01-26 20:45:15 -0800

    set up db and time zone

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -17,4 +17,6 @@ def index(request):
     return render(request, 'rango/index.html', context_dict)
     
 def about(request):
-    return HttpResponse("Rango says here is the about page. <br/> <a href='/rango'>Index</a> ")
+    context_dict = {'newmessage': "I am the new message"}
+
+    return render(request, 'rango/about.html', context_dict)
